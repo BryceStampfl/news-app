e1b09ff210f79fcb727ab31f668b31e98be8928b
Bryce Stampfl <brycestampfl@gmail.com>
2020-06-09 12:26:09 -0700

    Added some app wide styling and setup the component order.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit, Output} from '@angular/core';
+import {NewsApiService} from './services/news-api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'news-app';
+export class AppComponent implements OnInit {
+  news: any = [];
+  tabSelected: string;
+
+  constructor(private newsApi: NewsApiService) {
+    console.log('app component constructor called');
+  }
+
+  ngOnInit(): void {
+    this.showNews('business');
+  }
+
+  showNews(category: string) {
+    this.newsApi.getNews('us', category)
+      .subscribe(data => {
+        return this.news = Object.values(data)[2];
+      });
+  }
+
+  onTabSelected(tab: string) {
+    this.tabSelected = tab;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {NewsApiService} from './services/news-api.service';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import { HeaderComponent } from './header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { CardComponent } from './card/card.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { CardListOptionsComponent } from './card-list-options/card-list-options.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CardComponent,
+    CardListOptionsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatButtonModule,
   ],
-  providers: [],
+  providers: [NewsApiService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
